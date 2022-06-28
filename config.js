fe1d65d07b3dd7a4eee79710758e6c186f3a0abe
arizasaputri <108358632+arizasaputri@users.noreply.github.com>
2022-06-28 23:58:35 +0700

    Happy Birthday Ariza Saputri

---
+++ config.js
@@ -0,0 +1,81 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "Today is...",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "as beautiful as other days",  // 同上...
+        "but you realize",
+        "another year has gone",
+        "in a blink of the eyes",
+        "however",
+        "Do you know..?",
+        "today is just special",
+        "so special to you",
+        "that's why",
+        "Let's make it...",
+        "the best celebration ever",
+        "and let me share...",
+        "a piece of happiness to you",
+        "I made all this...",
+        "as a birthday present to you",
+        "thanks for being there",
+        "thanks for the friendship we made",
+        "thanks for everything",
+        "I wish you all the best",
+        "May your life be at ease",
+        "May all your wishes come true",
+        "Remember",
+        "your ambitions",
+        "you live as a free bird...",
+        "flying in the blue sky",
+        "Now things are different...",
+        "real story of your life",
+        "is just about to begin",
+        "indeed..",
+        "but...",
+        "don't worry",
+        "because...",
+        "God has your back",
+        "and",
+        "this year will be better",
+        "and I hope",
+        "you'll find...",
+        "happiness along the way",
+        "keep your spirit up",
+        "enjoy every single moment...",
+        "that you experience today",
+        "fill it with your most beautiful smile",
+        "and make it the best memory..",
+        "lastly...",
+        "I'd like to wish you one more time",
+        "a very happy birthday ARIZA SAPUTRI",
+        "ALIPP~~",
+
+    ],
+        
+    /**
+     * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
+     * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
+     * 例如
+     * "心爱的小可爱": "./imgs/xiaokeai.jpg"
+     *
+     * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
+     * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
+     */
+    imgs: {
+        "心爱的小可爱": "./imgs/xiaokeai.png",
+        // "今天是你的生日": "./imgs/birthday.jpg",
+    },
+    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    desc: {
+        turn_on: "Turn On Lights",
+        play: "Play Music",
+        bannar_coming: "Let's Decorate",
+        balloons_flying: "Fly With Balloons",
+        cake_fadein: "Most Delicious Cake Ever",
+        light_candle: "Light Candle",
+        wish_message: "Happy Birthday",
+        story: "A MESSAGE FOR YOU",
+    }
+};
